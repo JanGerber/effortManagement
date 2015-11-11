a57a849a376879ec0e56b5c5a6dd6f21c5d31669
lfnonnenmacher <lfnonnenmacher@SHO-NB069.schneeberger-it.com>
2015-11-11 18:03:46 +0100

    neue klassen

---
+++ src/main/java/com/effortmanagement/model/Semester.java
@@ -0,0 +1,70 @@
+package com.effortmanagement.model;
+public class Semester {
+
+	private int SemesterID;
+	private int Semestername;
+	private int Startdatum;
+	private int Enddatum;
+
+	public void getSemesterID() {
+		// TODO - implement Semester.getSemesterID
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param SemesterID
+	 */
+	public void setSemesterID(int SemesterID) {
+		// TODO - implement Semester.setSemesterID
+		throw new UnsupportedOperationException();
+	}
+
+	public void getSemestername() {
+		// TODO - implement Semester.getSemestername
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Semestername
+	 */
+	public void setSemestername(int Semestername) {
+		// TODO - implement Semester.setSemestername
+		throw new UnsupportedOperationException();
+	}
+
+	public void getStartdatum() {
+		// TODO - implement Semester.getStartdatum
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Startdatum
+	 */
+	public void setStartdatum(int Startdatum) {
+		// TODO - implement Semester.setStartdatum
+		throw new UnsupportedOperationException();
+	}
+
+	public void getEnddatum() {
+		// TODO - implement Semester.getEnddatum
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Enddatum
+	 */
+	public void setEnddatum(int Enddatum) {
+		// TODO - implement Semester.setEnddatum
+		throw new UnsupportedOperationException();
+	}
+
+	public Semester() {
+		// TODO - implement Semester.Semester
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/main/java/com/effortmanagement/model/Vorlesung.java
@@ -0,0 +1,145 @@
+package com.effortmanagement.model;
+public class Vorlesung {
+
+	private int VorlesungID;
+	private int Vorlesungsname;
+	private int Aufwand;
+	private int Note;
+	private int Eigeninitiative;
+	private int Lernzeit;
+	private int Notenziel;
+	private int Creditpoints;
+
+	public void getVorlesungID() {
+		// TODO - implement Vorlesung.getVorlesungID
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param VorlesungID
+	 */
+	public void setVorlesungID(int VorlesungID) {
+		// TODO - implement Vorlesung.setVorlesungID
+		throw new UnsupportedOperationException();
+	}
+
+	public void getVorlesungsname() {
+		// TODO - implement Vorlesung.getVorlesungsname
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Vorlesungsname
+	 */
+	public void setVorlesungsname(int Vorlesungsname) {
+		// TODO - implement Vorlesung.setVorlesungsname
+		throw new UnsupportedOperationException();
+	}
+
+	public void getAufwand() {
+		// TODO - implement Vorlesung.getAufwand
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Aufwand
+	 */
+	public void setAufwand(int Aufwand) {
+		// TODO - implement Vorlesung.setAufwand
+		throw new UnsupportedOperationException();
+	}
+
+	public void getNote() {
+		// TODO - implement Vorlesung.getNote
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Note
+	 */
+	public void setNote(int Note) {
+		// TODO - implement Vorlesung.setNote
+		throw new UnsupportedOperationException();
+	}
+
+	public void getEigeninitiative() {
+		// TODO - implement Vorlesung.getEigeninitiative
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Eigeninitiative
+	 */
+	public void setEigeninitiative(int Eigeninitiative) {
+		// TODO - implement Vorlesung.setEigeninitiative
+		throw new UnsupportedOperationException();
+	}
+
+	public void getLernzeit() {
+		// TODO - implement Vorlesung.getLernzeit
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Lernzeit
+	 */
+	public void setLernzeit(int Lernzeit) {
+		// TODO - implement Vorlesung.setLernzeit
+		throw new UnsupportedOperationException();
+	}
+
+	public void getNotenziel() {
+		// TODO - implement Vorlesung.getNotenziel
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Notenziel
+	 */
+	public void setNotenziel(int Notenziel) {
+		// TODO - implement Vorlesung.setNotenziel
+		throw new UnsupportedOperationException();
+	}
+
+	public void getCreditpoints() {
+		// TODO - implement Vorlesung.getCreditpoints
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Creditpoints
+	 */
+	public void setCreditpoints(int Creditpoints) {
+		// TODO - implement Vorlesung.setCreditpoints
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean ZielErreicht() {
+		// TODO - implement Vorlesung.ZielErreicht
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean Bestanden() {
+		// TODO - implement Vorlesung.Bestanden
+		throw new UnsupportedOperationException();
+	}
+
+	public int Erfolg() {
+		// TODO - implement Vorlesung.Erfolg
+		throw new UnsupportedOperationException();
+	}
+
+	public Vorlesung() {
+		// TODO - implement Vorlesung.Vorlesung
+		throw new UnsupportedOperationException();
+	}
+
+}
