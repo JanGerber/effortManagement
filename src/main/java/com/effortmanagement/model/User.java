a516be7f4f567903e4089f20afcf4b4a36e42cdb
lfnonnenmacher <lfnonnenmacher@SHO-NB069.schneeberger-it.com>
2015-11-11 17:54:32 +0100

    Befüllte Java Klasse User

--- src/main/java/com/effortmanagement/model/User.java
+++ src/main/java/com/effortmanagement/model/User.java
@@ -2,4 +2,85 @@ package com.effortmanagement.model;
 
 public class User {
 
+	private int UserID;
+	private int Username;
+	private int Passwort;
+	private int EMail;
+	private int Hochschule;
+
+	public void getUserID() {
+		// TODO - implement User.getUserID
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param UserID
+	 */
+	public void setUserID(int UserID) {
+		// TODO - implement User.setUserID
+		throw new UnsupportedOperationException();
+	}
+
+	public void getUsername() {
+		// TODO - implement User.getUsername
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Username
+	 */
+	public void setUsername(int Username) {
+		// TODO - implement User.setUsername
+		throw new UnsupportedOperationException();
+	}
+
+	public void getPasswort() {
+		// TODO - implement User.getPasswort
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Passwort
+	 */
+	public void setPasswort(int Passwort) {
+		// TODO - implement User.setPasswort
+		throw new UnsupportedOperationException();
+	}
+
+	public void getEMail() {
+		// TODO - implement User.getEMail
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param EMail
+	 */
+	public void setEMail(int EMail) {
+		// TODO - implement User.setEMail
+		throw new UnsupportedOperationException();
+	}
+
+	public void getHochschule() {
+		// TODO - implement User.getHochschule
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Hochschule
+	 */
+	public void setHochschule(int Hochschule) {
+		// TODO - implement User.setHochschule
+		throw new UnsupportedOperationException();
+	}
+
+	public User() {
+		// TODO - implement User.User
+		throw new UnsupportedOperationException();
+	}
+
 }
