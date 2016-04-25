6a7b81fe5ffd952d6c401b18c18dc537fb861127
Jan Gerber <jan-456@t-online.de>
2016-04-25 20:54:21 +0200

    Junit Test Klassen

---
+++ effortManagementRest/src/test/java/com/effortmanagement/controller/SemesterControllerTest.java
@@ -0,0 +1,54 @@
+package com.effortmanagement.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SemesterControllerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetSemesterList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSemester() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteSemester() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangeSemester() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNewSemester() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ effortManagementRest/src/test/java/com/effortmanagement/controller/VorlesungControllerTest.java
@@ -0,0 +1,43 @@
+package com.effortmanagement.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class VorlesungControllerTest {
+
+	@Test
+	public void testCreateVorlesung() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNotenList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetVorlesung() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBucheAufwand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAufwandById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetListVorlesung() {
+		fail("Not yet implemented");
+	}
+
+}
