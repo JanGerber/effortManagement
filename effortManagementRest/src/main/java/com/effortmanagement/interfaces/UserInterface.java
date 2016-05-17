380335353dc2e35fc375e640ffdfce4b2fc25d06
siminalbombe <hofmannsimon94@gmail.com>
2016-05-17 19:00:39 +0200

    refactor interface user and userservice

--- effortManagementRest/src/main/java/com/effortmanagement/interfaces/UserInterface.java
+++ effortManagementRest/src/main/java/com/effortmanagement/interfaces/UserInterface.java
@@ -3,9 +3,17 @@ package com.effortmanagement.interfaces;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
+import com.effortmanagement.model.CreateUser;
+import com.effortmanagement.model.User;
+
 public interface UserInterface {
 	
 	//only changes of database entries implemented
 	//new entries should be added
 	public int changeData(int userId, String column, String newData);
+	public int newUser(CreateUser user);
+	public User getUserByName(String userName);
+	public String getPasswort(String userName);
+	public User getUserById(int userID);
+	public String getPasswort(int userId);
 }
